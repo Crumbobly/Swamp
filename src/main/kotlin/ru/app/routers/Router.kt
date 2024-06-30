8a700e538a39b401002bc001bcb7c3531d12b7d6
Crumbobly <vkvalera004@gmail.com>
2024-06-30 16:59:01 +0300

    Добавлен отдельный класс роутера

--- src/main/kotlin/ru/app/routers/Router.kt
+++ src/main/kotlin/ru/app/routers/Router.kt
@@ -1,8 +1,10 @@
 package ru.app.routers
 
 import org.http4k.core.Method
+import org.http4k.routing.ResourceLoader
 import org.http4k.routing.bind
 import org.http4k.routing.routes
+import org.http4k.routing.static
 import ru.app.web.handlers.WelcomePageHandler
 import ru.lab1.web.templates.ContextAwareViewRender
 
@@ -10,7 +12,7 @@ class Router {
 
     fun router(htmlView: ContextAwareViewRender) = routes(
         "/" bind Method.GET to WelcomePageHandler(htmlView),
-
+        static(ResourceLoader.Classpath("/ru/app/public")),
         )
 
 }
