bf0faf322ad83aa9ee04fb27cb4dd555dfb8c3a5
Crumbobly <vkvalera004@gmail.com>
2024-07-02 16:23:21 +0300

    Frontend-им потихоньку

--- src/main/kotlin/ru/app/web/handlers/WelcomePageHandler.kt
+++ src/main/kotlin/ru/app/web/handlers/WelcomePageHandler.kt
@@ -3,9 +3,11 @@ package ru.app.web.handlers
 import org.http4k.core.*
 import ru.app.web.models.WelcomePageVM
 import ru.lab1.web.templates.ContextAwareViewRender
+import java.time.LocalDateTime
 
 class WelcomePageHandler(private val htmlView: ContextAwareViewRender) : HttpHandler {
     override fun invoke(request: Request): Response {
-        return Response.invoke(Status.OK).with(htmlView(request) of WelcomePageVM())
+        val dateTime = LocalDateTime.now()
+        return Response.invoke(Status.OK).with(htmlView(request) of WelcomePageVM(dateTime))
     }
 }
