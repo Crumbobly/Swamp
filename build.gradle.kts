21630de5ec88e034579044cb45523cf406065af9
Crumbobly <vkvalera004@gmail.com>
2024-07-10 21:40:07 +0300

    Добавлены новые зависимости

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ val http4kVersion: String by project
 val http4kConnectVersion: String by project
 val junitVersion: String by project
 val kotlinVersion: String by project
+val exposedVersion: String by project
 
 application {
     mainClass = "ru.app.WhisperKt"
@@ -52,15 +53,31 @@ tasks {
 }
 
 dependencies {
+
+//    http4k
     implementation("org.http4k:http4k-client-okhttp:${http4kVersion}")
     implementation("org.http4k:http4k-core:${http4kVersion}")
     implementation("org.http4k:http4k-format-jackson:${http4kVersion}")
     implementation("org.http4k:http4k-server-netty:${http4kVersion}")
     implementation("org.http4k:http4k-template-pebble:${http4kVersion}")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
-    testImplementation("org.http4k:http4k-testing-approval:${http4kVersion}")
-    testImplementation("org.http4k:http4k-testing-hamkrest:${http4kVersion}")
+    implementation("org.http4k:http4k-multipart:${http4kVersion}")
+    implementation(platform("org.http4k:http4k-bom:5.25.0.0"))
+    implementation("org.http4k:http4k-cloudnative")
     testImplementation("org.http4k:http4k-testing-kotest:${http4kVersion}")
+    implementation("org.http4k:http4k-format-kotlinx-serialization:${http4kVersion}")
+
+
+
+//    db
+    implementation("org.jetbrains.exposed:exposed-core:${exposedVersion}")
+    implementation("org.jetbrains.exposed:exposed-dao:${exposedVersion}")
+    implementation("org.jetbrains.exposed:exposed-jdbc:${exposedVersion}")
+    implementation("com.zaxxer:HikariCP:5.0.0")
+    implementation("mysql:mysql-connector-java:8.0.27")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.8.0")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
